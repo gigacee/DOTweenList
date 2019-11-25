5399da945621f59ec10b412d67aeed83ea2e6ec6
Gigacee <t47833@gmail.com>
2019-11-25 23:51:43 +0900

    DOTweenList およびサンプルシーンを追加

---
+++ Assets/DOTweenList/Examples/DOTweenList_Example.cs
@@ -0,0 +1,77 @@
+﻿using DG.Tweening;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DOTweenList_Example : MonoBehaviour
+{
+    public Text message;
+
+    public RectTransform rectA;
+    public RectTransform rectB;
+    public RectTransform rectC;
+
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+
+    DOTweenList dotweenList;
+
+    void Awake()
+    {
+        DOTween.Init();
+        DOTween.defaultAutoPlay = AutoPlay.None;
+        DOTween.defaultAutoKill = false;
+    }
+
+    void Start()
+    {
+        dotweenList = new DOTweenList(
+            rectA.DOAnchorPosX(500f, 0.6f),
+            rectB.DOAnchorPosX(500f, 0.8f).SetId("idTest"),
+            rectC.DOAnchorPosX(500f, 1f)
+        );
+
+        button1.onClick.AddListener(async() => await PlayTweensForwardAsync());
+        button2.onClick.AddListener(async() => await PlayTweensBackwardsAsync());
+        button3.onClick.AddListener(async() => await PlayTweensForwardByIdAsync());
+        button4.onClick.AddListener(async() => await PlayTweensBackwardsByIdAsync());
+    }
+
+    async UniTask PlayTweensForwardAsync()
+    {
+        message.text = "Playing Forwards...";
+
+        await dotweenList.PlayForward();
+
+        message.text = "All tweens completed.";
+    }
+
+    async UniTask PlayTweensBackwardsAsync()
+    {
+        message.text = "Playing Backwards...";
+
+        await dotweenList.PlayBackwards();
+
+        message.text = "All tweens rewinded.";
+    }
+
+    async UniTask PlayTweensForwardByIdAsync()
+    {
+        message.text = "Playing Forwards...";
+
+        await dotweenList.PlayForwardById("idTest");
+
+        message.text = "Tween B completed.";
+    }
+
+    async UniTask PlayTweensBackwardsByIdAsync()
+    {
+        message.text = "Playing Backwards...";
+
+        await dotweenList.PlayBackwardsById("idTest");
+
+        message.text = "Tween B rewinded.";
+    }
+}
---
+++ Assets/DOTweenList/Scripts/DOTweenList.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UniRx.Async;
+using UnityEngine;
+
+public class DOTweenList : CustomYieldInstruction
+{
+	List<Tween> list;
+	public Func<bool> cond;
+
+	public DOTweenList(params Tween[] tweens)
+	{
+		list = tweens.ToList();
+	}
+
+	public DOTweenList(IEnumerable<Tween> tweens)
+	{
+		list = tweens.Where(x => x != null).ToList();
+	}
+
+	public override bool keepWaiting
+	{
+		get
+		{
+			if ((cond != null && cond()))
+			{
+				foreach (var tween in list)
+				{
+					if (tween.IsActive() && tween.IsPlaying())
+					{
+						if (tween.IsBackwards())
+						{
+							tween.Rewind();
+						}
+						else
+						{
+							tween.Complete();
+						}
+					}
+				}
+
+				return false;
+			}
+
+			return IsPlaying();
+		}
+	}
+
+	public DOTweenList PlayForward(bool rewind = false)
+	{
+		foreach (var tween in list)
+		{
+			if (rewind)
+			{
+				tween.Rewind();
+			}
+
+			tween.PlayForward();
+		}
+
+		return this;
+	}
+
+	public DOTweenList PlayForwardById(string id, bool rewind = false)
+	{
+		foreach (var tween in list)
+		{
+			if (tween.stringId == id)
+			{
+				if (rewind)
+				{
+					tween.Rewind();
+				}
+
+				tween.PlayForward();
+			}
+		}
+
+		return this;
+	}
+
+	public DOTweenList PlayBackwards(bool complete = false)
+	{
+		foreach (var tween in list)
+		{
+			if (complete)
+			{
+				tween.Complete();
+			}
+
+			tween.PlayBackwards();
+		}
+
+		return this;
+	}
+
+	public DOTweenList PlayBackwardsById(string id, bool complete = false)
+	{
+		foreach (var tween in list)
+		{
+			if (tween.stringId == id)
+			{
+				if (complete)
+				{
+					tween.Complete();
+				}
+
+				tween.PlayBackwards();
+			}
+		}
+
+		return this;
+	}
+
+	public void Rewind()
+	{
+		foreach (var tween in list)
+		{
+			tween.Rewind();
+		}
+	}
+
+	public void RewindById(string id)
+	{
+		foreach (var tween in list)
+		{
+			if (tween.stringId == id)
+			{
+				tween.Rewind();
+			}
+		}
+	}
+
+	public void Complete()
+	{
+		foreach (var tween in list)
+		{
+			tween.Complete();
+		}
+	}
+
+	public void CompleteById(string id)
+	{
+		foreach (var tween in list)
+		{
+			if (tween.stringId == id)
+			{
+				tween.Complete();
+			}
+		}
+	}
+
+	public void Kill()
+	{
+		foreach (var tween in list)
+		{
+			tween.Kill();
+		}
+	}
+
+	public void KillById(string id)
+	{
+		foreach (var tween in list)
+		{
+			if (tween.stringId == id)
+			{
+				tween.Kill();
+			}
+		}
+	}
+
+	public bool IsPlaying() => list.Any(x => x.IsActive() && x.IsPlaying());
+
+	public void Add(Tween tween) => list.Add(tween);
+
+	public void Clear() => list.Clear();
+
+	public float GetTotalTime() => list.Max(x => x.Duration() + x.Delay());
+
+	public float GetTotalTimeById(string id) =>
+		list.Where(x => x.stringId == id).Max(x => x.Duration() + x.Delay());
+}
+
+public static class DOTweenListExtensions
+{
+	public static Tween AddTo(this Tween tween, DOTweenList dotweenList)
+	{
+		if (tween == null)
+		{
+			throw new ArgumentNullException("tween");
+		}
+
+		if (dotweenList == null)
+		{
+			throw new ArgumentNullException("dotweenList");
+		}
+
+		dotweenList.Add(tween);
+
+		return tween;
+	}
+
+	public static DOTweenList Skippable(this DOTweenList self, Func<bool> cond)
+	{
+		self.cond = cond;
+
+		return self;
+	}
+
+	public static async void Forget(this DOTweenList self)
+	{
+		try
+		{
+			await self;
+		}
+		catch (OperationCanceledException) { }
+	}
+}
