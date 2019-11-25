aeedc7fd0d81b982806f10ff951ba2e94f1413f6
Gigacee <t47833@gmail.com>
2019-11-26 01:25:56 +0900

    サンプルを微修正、スペースをタブに変換、README を記述

--- Assets/DOTweenList/Examples/DOTweenList_Example.cs
+++ Assets/DOTweenList/Examples/DOTweenList_Example.cs
@@ -5,73 +5,73 @@ using UnityEngine.UI;
 
 public class DOTweenList_Example : MonoBehaviour
 {
-    public Text message;
+	public Text message;
 
-    public RectTransform rectA;
-    public RectTransform rectB;
-    public RectTransform rectC;
+	public RectTransform rectA;
+	public RectTransform rectB;
+	public RectTransform rectC;
 
-    public Button button1;
-    public Button button2;
-    public Button button3;
-    public Button button4;
+	public Button button1;
+	public Button button2;
+	public Button button3;
+	public Button button4;
 
-    DOTweenList dotweenList;
+	DOTweenList dotweenList;
 
-    void Awake()
-    {
-        DOTween.Init();
-        DOTween.defaultAutoPlay = AutoPlay.None;
-        DOTween.defaultAutoKill = false;
-    }
+	void Awake()
+	{
+		DOTween.Init();
+		DOTween.defaultAutoPlay = AutoPlay.None;
+		DOTween.defaultAutoKill = false;
+	}
 
-    void Start()
-    {
-        dotweenList = new DOTweenList(
-            rectA.DOAnchorPosX(500f, 0.6f),
-            rectB.DOAnchorPosX(500f, 0.8f).SetId("idTest"),
-            rectC.DOAnchorPosX(500f, 1f)
-        );
+	void Start()
+	{
+		dotweenList = new DOTweenList(
+			rectA.DOAnchorPosX(600f, 0.6f),
+			rectB.DOAnchorPosX(600f, 0.8f).SetId("idTest"),
+			rectC.DOAnchorPosX(600f, 1f)
+		);
 
-        button1.onClick.AddListener(async() => await PlayTweensForwardAsync());
-        button2.onClick.AddListener(async() => await PlayTweensBackwardsAsync());
-        button3.onClick.AddListener(async() => await PlayTweensForwardByIdAsync());
-        button4.onClick.AddListener(async() => await PlayTweensBackwardsByIdAsync());
-    }
+		button1.onClick.AddListener(async() => await PlayTweensForwardAsync());
+		button2.onClick.AddListener(async() => await PlayTweensBackwardsAsync());
+		button3.onClick.AddListener(async() => await PlayTweensForwardByIdAsync());
+		button4.onClick.AddListener(async() => await PlayTweensBackwardsByIdAsync());
+	}
 
-    async UniTask PlayTweensForwardAsync()
-    {
-        message.text = "Playing Forwards...";
+	async UniTask PlayTweensForwardAsync()
+	{
+		message.text = "Playing Forwards...";
 
-        await dotweenList.PlayForward();
+		await dotweenList.PlayForward();
 
-        message.text = "All tweens completed.";
-    }
+		message.text = "All tweens completed.";
+	}
 
-    async UniTask PlayTweensBackwardsAsync()
-    {
-        message.text = "Playing Backwards...";
+	async UniTask PlayTweensBackwardsAsync()
+	{
+		message.text = "Playing Backwards...";
 
-        await dotweenList.PlayBackwards();
+		await dotweenList.PlayBackwards();
 
-        message.text = "All tweens rewinded.";
-    }
+		message.text = "All tweens rewinded.";
+	}
 
-    async UniTask PlayTweensForwardByIdAsync()
-    {
-        message.text = "Playing Forwards...";
+	async UniTask PlayTweensForwardByIdAsync()
+	{
+		message.text = "Playing Forwards...";
 
-        await dotweenList.PlayForwardById("idTest");
+		await dotweenList; //.PlayForwardById("idTest");
 
-        message.text = "Tween B completed.";
-    }
+		message.text = "Tween B completed.";
+	}
 
-    async UniTask PlayTweensBackwardsByIdAsync()
-    {
-        message.text = "Playing Backwards...";
+	async UniTask PlayTweensBackwardsByIdAsync()
+	{
+		message.text = "Playing Backwards...";
 
-        await dotweenList.PlayBackwardsById("idTest");
+		await dotweenList.PlayBackwardsById("idTest");
 
-        message.text = "Tween B rewinded.";
-    }
+		message.text = "Tween B rewinded.";
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,122 @@
 # DOTweenList
-DOTween で複数の Tween をまとめて処理します。
+
+DOTween の複数の Tween をまとめて再生したり、待機したりできるようになります。
+
+## 注意
+
+以下のアセットが別途必要です。
+
+- [DOTween](https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676)
+- [UniTask](https://github.com/Cysharp/UniTask)
+
+## 使い方
+
+### 準備
+
+まず、DOTweenList に処理したい Tween を詰めます。以下の 3 つの方法があります：
+
+(i) コンストラクター
+
+```cs
+var dotweenList = new DOTweenList(
+	rectA.DOAnchorPosX(600f, 0.6f),
+	rectB.DOAnchorPosX(600f, 0.8f),
+	rectC.DOAnchorPosX(600f, 1f)
+);
+```
+
+(ii) `Add()`メソッド
+
+```cs
+var dotweenList = new DOTweenList();
+
+dotweenList.Add(rectA.DOAnchorPosX(600f, 0.6f));
+dotweenList.Add(rectB.DOAnchorPosX(600f, 0.8f));
+dotweenList.Add(rectC.DOAnchorPosX(600f, 1f));
+```
+
+(iii) `AddTo()`拡張メソッド
+
+```cs
+var dotweenList = new DOTweenList();
+
+rectA.DOAnchorPosX(600f, 0.6f).AddTo(dotweenList);
+rectB.DOAnchorPosX(600f, 0.8f).AddTo(dotweenList);
+rectC.DOAnchorPosX(600f, 1f).AddTo(dotweenList);
+```
+
+### 処理を実行
+
+DOTweenList には、以下の処理を実行できます。
+
+```cs
+// 再生
+dotweenList.PlayForward();
+
+// 逆再生
+dotweenList.PlayBackwards();
+
+// 最初に戻す
+dotweenList.Rewind();
+
+// 最後に進める
+dotweenList.Complete();
+
+// 強制終了
+dotweenList.Kill();
+```
+
+メソッド名の末尾に`ById`を加えることで、特定の ID の Tween のみに処理を実行できます。
+
+```cs
+dotweenList.PlayForwardById("id");
+
+dotweenList.PlayBackwardsById("id");
+
+dotweenList.RewindById("id");
+
+dotweenList.CompleteById("id");
+
+dotweenList.KillById("id");
+```
+
+この他、以下のメソッドが用意されています。
+
+```cs
+// Tween がどれか一つでも再生中なら true を返す
+dotweenList.IsPlaying();
+
+// リストをクリアする
+dotweenList.Clear();
+
+// すべての Tween の再生開始から終了までにかかる時間を取得する
+// 注意：Tween が再生中であっても、開始からの時間が返る
+dotweenList.GetTotalTime();
+
+// 特定の ID を持つすべての Tween の再生にかかる時間を取得する
+// 注意：Tween が再生中であっても、開始からの時間が返る
+dotweenList.GetTotalTimeById("id");
+```
+
+### 待機
+
+`await`を付与することで、再生終了まで待機することができます。
+
+```cs
+// 再生終了まで待機する
+await dotweenList.PlayForward();
+
+// すでに再生中の Tween を待つこともできる
+await dotweenList;
+```
+
+`Skippable()`拡張メソッドを用いれば、指定した条件で Tween をスキップすることができます。
+
+```cs
+// 再生終了まで待機する。何かキーを入力するとスキップする
+await dotweenList.PlayForward().Skippable(() => Input.anyKeyDown);
+```
+
+## DOTween Animation
+
+DOTween Pro の機能である DOTween Animation を DOTweenList に詰める場合は、以下のように書きます。
